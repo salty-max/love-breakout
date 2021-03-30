dac98ea281e00d6755e30086f5ce0ab97ce89293
max-jellycat <max@jellycat.fr>
2021-03-30 22:29:10 +0200

    Initial commit :wave:

---
+++ main.lua
@@ -0,0 +1,27 @@
+--[[
+    MAIN PROGRAM
+    CS50G Project 3
+    Breakout
+    Author: Maxime Blanc
+    https://github.com/salty-max
+--]]
+
+--[[
+    Library to set virtual resolution
+    -- https://github.com/Ulydev/push
+--]]
+push = require 'lib/push'
+
+--[[
+    Library to implement a class system in lua
+    -- https://github.com/vrld/hump/blob/master/class.lua
+--]]
+Class = require 'lib/class'
+
+function love.load()
+end
+
+function love.update(dt)
+
+function love.draw()
+end
